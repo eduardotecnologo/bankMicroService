4339ac581e5e166906b6cf0ef3d1a3eeb2d0c1ad
Eduardo Developer <eduardotecnologo@hotmail.com>
2022-08-21 12:55:32 -0300

    Start project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bankMicroService
 bankMicroService
+
+# docker composer -d
+# docker exec -it appbank bash
+- root@b3f01a281298:/go/src# go mod init github.com/eduardotecnologo/bankMicroService
---
+++ codebank/domain/transaction.go
@@ -0,0 +1,40 @@
+package domain
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// Persistencies
+type TransactionRepository interface {
+	SaveTransaction(transaction Transaction, creditCard CreditCard) error
+	GetTransaction(creditCard CreditCard) (CreditCard, error)
+	CreateCreditCard(creditCard CreditCard) error
+}
+
+type Transaction struct {
+	ID           string
+	Amount       float64
+	Description  string
+	Status       string
+	Store        string
+	CreditCardId string
+	CreatedAt    time.Time
+}
+
+func NewTransaction() *Transaction {
+	t := &Transaction{}
+	t.ID = uuid.NewV4().String()
+	t.CreatedAt = time.Now().UTC()
+	return t
+}
+
+func (t *Transaction) ProcessAndValidate(creditCard *CreditCard) {
+	if t.Amount+creditCard.Balance > creditCard.Limit {
+		t.Status = "rejected"
+	} else {
+		t.Status = "approved"
+		creditCard.Balance = creditCard.Balance + t.Amount
+	}
+}
